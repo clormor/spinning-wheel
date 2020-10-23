a4cd04566ee09e31aaa2e8bd23b2812fa5279feb
Chris Lormor <clormor@palantir.com>
2020-10-23 15:04:13 +0100

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# Spinning Wheel
-
-A spinning wheel!
-
-The result can be seen [here][github-pages].
-
-[github-pages]: https://clormor.github.io/spinning-wheel/
---
+++ precache-manifest.53c5cce26c9c9e8b851256f5f13c470d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fabaf5812676e0cbf619fe5cc55e3bf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea6df903e3512e3f1f81",
+    "url": "/static/css/main.cc6b354d.chunk.css"
+  },
+  {
+    "revision": "4970dcccf2d8f5189bed",
+    "url": "/static/js/2.67068186.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.67068186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea6df903e3512e3f1f81",
+    "url": "/static/js/main.f8028c90.chunk.js"
+  },
+  {
+    "revision": "e3febd812283afcbe160",
+    "url": "/static/js/runtime-main.955ea2f7.js"
+  },
+  {
+    "revision": "1f3cc587fa7b6ab3512d3fa8dd153667",
+    "url": "/static/media/arrow-left.1f3cc587.svg"
+  }
+]);
